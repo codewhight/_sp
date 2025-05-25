77210ca0f4fb271777610bfc317f57a671899910
codewhight <161123216+codewhight@users.noreply.github.com>
2025-05-26 01:05:39 +0800

    Delete h4 directory

--- h4/mul3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int mul3(int a, int b, int c);
-
-
-int main()
-{
-    printf("mul3(5, 2, 3) = %d\n", mul3(5, 2, 3));
-}
--- h4/mul3.s
+++
@@ -1,9 +0,0 @@
-.globl  mul3
-        
-        .text
-mul3:
-        mov     %rdi, %rax                
-        imul   %rsi, %rax                
-        imul   %rdx, %rax               
-                     
-        ret                               
